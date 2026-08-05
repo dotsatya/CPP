0a0b6453004bf9c52c860ba8305f70175f16686f
dotsatya <satyasundardey5@gmail.com>
2026-08-05 19:58:48 +0530

    add abbstraction

--- OOPs-Cpp/Encapsulation/ex.cpp
+++ OOPs-Cpp/Encapsulation/ex.cpp
@@ -31,3 +31,44 @@ int main() {
 
     cout << acc.getBalance();
 }
+
+
+
+/* 
+You have a TV with a remote.
+
+You press:
+
+Power ON
+Volume +
+Volume -
+
+Do you know how the TV turns on internally?
+
+No.
+
+You only know what to press.
+
+This is Abstraction.
+
+Abstraction = Hiding HOW something works.
+
+Now think about the TV's internal parts
+
+Inside the TV there are:
+
+Circuit board
+Wires
+Chips
+Memory
+
+Can you directly change them?
+
+No.
+
+The TV protects them.
+
+This is Encapsulation.
+
+Encapsulation = Protecting data from direct access.
+*/
